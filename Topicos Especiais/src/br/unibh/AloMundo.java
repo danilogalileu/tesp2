013f32214aebfa9d171a09ec9e4ea44da7454c8e
Danilo <Danilo@Danilo>
2015-03-12 10:32:28 -0300

    Atualizacao do projeto

--- Topicos Especiais/src/br/unibh/AloMundo.java
+++ Topicos Especiais/src/br/unibh/AloMundo.java
@@ -1,45 +1,55 @@
 package br.unibh;
 
-
 import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 
 import br.unibh.entidades.Aluno;
 import br.unibh.entidades.Professor;
+import br.unibh.persistencias.JDBCUtil;
 
 public class AloMundo {
 
 	public static Long numero;
-	
-	public static void main(String[] args) {
-		
-		Aluno a1 = new Aluno(new Long(1), new Long (1234), "João", "987987987", new Date());
-		Aluno a2 = new Aluno(new Long(2), new Long (5789), "João", "234234234", new Date());
-		Aluno a3 = new Aluno(new Long(3), new Long (1357), "Pablo", "321654987", new Date());
-		Aluno a4 = new Aluno(new Long(4),7193L,"Natã");
-		Professor p1 = new Professor (new Long(5),"Tite","123456789", new BigDecimal(123456));
-		Professor p2 = new Professor(new Long(6),"Cuca", "11058974125");
 
-		System.out.println("BONUS ==== "+Professor.BONUS);
-		System.out.println(Aluno.verificaMatricula("123123123"));
-		
+	public static void main(String[] args) {
+
+		Aluno a1 = new Aluno(new Long(1), new Long(1234), "Danilo",
+				"12345678911", new Date());
+		Aluno a2 = new Aluno(new Long(2), new Long(5789), "Bruno",
+				"11987654321", new Date());
+		Aluno a3 = new Aluno(new Long(3), new Long(1357), "Gustavo",
+				"98321654987", new Date());
+		Aluno a4 = new Aluno(new Long(4), 7193L, "Natã");
+		Professor p1 = new Professor(new Long(5), "Tite", "98765432561",
+				new BigDecimal(123456));
+		Professor p2 = new Professor(new Long(6), "Cuca", "90185142810");
+
+		System.out.println("BONUS ==== " + Professor.BONUS);
+		System.out.println(Aluno.verificaMatricula("11314491"));
+
 		System.out.println(a1);
 		System.out.println(a2);
 		System.out.println(a3);
 		System.out.println(a4);
 		System.out.println(p1);
 		System.out.println(p2);
-		
-		
-		/*int i =0;
-		
-		while (i < 10) {
-		
-		System.out.println(i+1+ " Alo Mundo.");
-		i++;
-		
-			}*/
-		
-		
+
+		try {
+			ResultSet res = JDBCUtil.getConnection()
+					.prepareStatement("select * from tb_aluno").executeQuery();
+
+			while (res.next()) {
+				System.out.println(res.getLong("id") + "\t"
+						+ res.getString("nome"));
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
 	}
 }
