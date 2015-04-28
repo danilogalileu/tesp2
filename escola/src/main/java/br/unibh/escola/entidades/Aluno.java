2707263d95e754432c66e25416523d04db0168b4
Danilo <Danilo@Danilo>
2015-04-28 18:50:40 -0300

    Exercicio 05 finalizado

--- escola/src/main/java/br/unibh/escola/entidades/Aluno.java
+++ escola/src/main/java/br/unibh/escola/entidades/Aluno.java
@@ -4,18 +4,24 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
-@Table(name = "TB_ALUNO")
+@PrimaryKeyJoinColumn
+@Table(name = "TB_ALUNO", uniqueConstraints = @UniqueConstraint(columnNames = "matricula"))
+@NamedQueries({ @NamedQuery(name = "Aluno.findByName", query = "select a from Aluno a where a.nome like :nome") })
 public class Aluno extends Pessoa {
 
-	@NotBlank
+	
 	@NotNull
 	@Column(unique = true)
 	private Long matricula;
@@ -26,7 +32,6 @@ public class Aluno extends Pessoa {
 	private Date dataAniversario;
 
 	public Aluno() {
-
 	}
 
 	public Aluno(Long id, Long matricula, String nome, String cpf,
@@ -81,4 +86,5 @@ public class Aluno extends Pessoa {
 		return super.toString() + "Aluno [matricula=" + matricula
 				+ ", dataAniversario=" + dataAniversario + "]";
 	}
+
 }
--- escola/src/main/java/br/unibh/escola/entidades/Professor.java
+++ escola/src/main/java/br/unibh/escola/entidades/Professor.java
@@ -4,13 +4,19 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "TB_PROFESSOR")
+@PrimaryKeyJoinColumn
+@Table(name = "TB_PROFESSOR", uniqueConstraints = @UniqueConstraint(columnNames = "salario"))
+@NamedQueries({ @NamedQuery(name = "Professor.findByName", query = "select a from Professor a where a.nome like :nome") })
 public class Professor extends Pessoa {
 
 	@NotNull
